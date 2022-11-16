4e035d5ac535efd91a56d8e96c18b320b30d315f
Jonatas Brisotti <jonatas.brisotti@gmail.com>
2022-11-16 10:55:41 +0100

    Initial Commit

---
+++ beercalc/beercalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  beercalcApp.swift
+//  beercalc
+//
+//  Created by Jonatas Brisotti on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct beercalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
