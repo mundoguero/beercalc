9c5ad428139f456c60d560b25b86e87624bd5b96
Jonatas Brisotti <jonatas.brisotti@gmail.com>
2022-11-23 12:59:42 +0100

    Adding some layout to the result view

--- beercalc/ResultView.swift
+++ beercalc/ResultView.swift
@@ -11,7 +11,28 @@ struct PushedView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("Yeah!!!")
+                HStack {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 400, height: 200)
+                            .opacity(0.2)
+                        Text("Result")
+                    }
+                }
+                HStack {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 198, height: 200)
+                        .opacity(0.2)
+                        Text("Result 2")
+                    }
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 198, height: 200)
+                        .opacity(0.2)
+                        Text("Result 3")
+                    }
+                }
             }
         }
         .navigationBarTitle("Pushed View")
